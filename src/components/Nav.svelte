00620426d81ebda0472ddd4ee0826416cf20e54d
Bedir Aygun <Bediruna@hotmail.com>
2024-06-10 16:14:58 -0400

    added basic nav

---
+++ src/components/Nav.svelte
@@ -0,0 +1,26 @@
+<nav>
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/resume">resume</a></li>
+        <li><a href="/work">work</a></li>
+    </ul>
+</nav>
+
+<style>
+    /* Your styles here */
+    nav ul {
+        list-style-type: none; /* Remove bullet points */
+        padding: 0; /* Remove default padding */
+        margin: 0; /* Remove default margin */
+        display: flex; /* Use flexbox to align items horizontally */
+    }
+
+    nav ul li {
+        margin-right: 20px; /* Add some spacing between items */
+    }
+
+    nav ul li a {
+        text-decoration: none;
+        color: white;
+    }
+</style>
