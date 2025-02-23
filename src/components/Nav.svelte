fec6f65b5ec6e0c84f75e8a07ccc345351ff6e40
Bedir Aygun <Bediruna@hotmail.com>
2025-02-23 14:21:15 +0000

    Added linkedin link

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,6 +4,7 @@
 
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
+        <!-- Left nav items -->
         <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-3">
             <li class="nav-item">
                 <a class="nav-link" href="/">BEDIR</a>
@@ -11,11 +12,24 @@
             <li class="nav-item">
                 <a class="nav-link" href="/resume">RESUME</a>
             </li>
-            <!-- <li class="nav-item">
-                <a class="nav-link" href="/work">Work</a>
-            </li> -->
         </ul>
-        <ThemeToggle />
+
+        <!-- Right nav items -->
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-3">
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="https://www.linkedin.com/in/bedir-aygun"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <ThemeToggle />
+            </li>
+        </ul>
     </div>
 </nav>
 
